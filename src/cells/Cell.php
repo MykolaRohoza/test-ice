d8e09089c19d7987c0f16e6219fdf46e6c0228cd
MykolaRohoza <rohoza_mykola@rambler.ru>
2018-03-13 20:24:48 +0200

    Работает

--- src/cells/Cell.php
+++ src/cells/Cell.php
@@ -7,24 +7,34 @@
 		
 		protected $cols; 
 		protected $rows; 
-		protected $different_cells = []; 
+		protected $different_cols = []; 
 		protected $different_rows = []; 
 		protected $text = "";
+		protected $coords = []; 
 		/**
 		 * cells
 		 */
-		protected $places = array(); 
+		protected $places = []; 
+		protected $min; 
 		protected $align = "inherit";
 		protected $valign = "inherit";
 		protected $color = "inherit";
 		protected $bgcolor = "inherit";
+		
 		protected $colspan = 1;
 		protected $rowspan = 1;
+		protected $width;
+		protected $height;
+		protected $top;
+		protected $left;
+		
+		
 		public function __construct($data, $places, $rows = 3, $cols = 3)
 		{
 			$this->rows = $rows;
 			$this->cols = $cols;
 			$this->init($data, $places);
+			
 		}
 		
 		protected function init($data, $places)
@@ -35,13 +45,95 @@
 			} else {
 				die('<br> $places ' . print_r($places, true) . ' - ' . print_r($this->errors, true) . '<br>');
 			}
+			
 			foreach ($data as $key => $value) {
 				if(property_exists($this, $key)){
 					$this->{$key} = $value;
 				} 
 			}
+			if(isset($data['valign'])){
+				$this->valign = $this->getValidValign($data['valign']);
+			}
+			if(isset($data['align'])){
+				$this->align = $this->getValidAlign($data['align']);
+			}
+			if(isset($data['color'])){
+				$this->align = $this->getValidAlign($data['align']);
+			}
+			
+			$this->colspan = count($this->different_cols);
+			$this->rowspan = count($this->different_rows);
+			
+			
+			$this->width = $this->getActualWidth();
+			$this->height = $this->getActualHeight();
+			$this->setActualMin();
+			$this->top = $this->getActualTop();
+			$this->left = $this->getActualLeft();
+		}
+		
+		protected function getValidValign($valign)
+		{
+			switch ($valign) {
+				case 'top':
+					return 'flex-start';	
+				case 'middle':
+					return 'center';	
+				case 'bottom':
+					return 'flex-end';	
+				default:
+					return $this->valign;
+			}
+		}
+		protected function getValidAlign($align)
+		{
+			switch ($align) {
+				case 'left':
+					return 'flex-start';	
+				case 'center':
+					return 'center';	
+				case 'right':
+					return 'flex-end';	
+				default:
+					return $this->valign;
+			}
+		}
+
+		
+
+
+
+		protected function getActualTop()
+		{
+			$result = $this->coords[$this->min]['num_row'] - 1;
+			
+			
+//			return $result * 100 / $this->rows . '%';
+			return $result * 300 / $this->rows . 'px';
+		}
+		protected function getActualLeft()
+		{			
+			$result = $this->coords[$this->min]['num_col'];
+			
+//			return $result * 100 / $this->cols . '%';
+			return $result * 300 / $this->cols . 'px';
 			
 		}
+		protected function getActualWidth()
+		{
+//			$result = (floor(100 / $this->cols) * $this->colspan);
+//			return $result . '%';
+			$result = (100 - 1) * $this->colspan;
+			return $result . 'px';
+		}
+		protected function getActualHeight()
+		{
+//			$result = (floor(100 / $this->rows)) * $this->rowspan;
+//			return $result . '%';
+			$result = (100 - 1) * $this->rowspan;
+			return $result . 'px';
+		}
+		
 		protected function checkPlaces($places)
 		{
 			if(!is_array($places)){
@@ -62,7 +154,7 @@
 		{
 			$this->setDifferentSides($places);
 
-			if(count($places) != (count($this->different_cells) * count($this->different_rows))){
+			if(count($places) != (count($this->different_cols) * count($this->different_rows))){
 				$this->errors[] = 'Произведение колличества отличающихся строк и колличества отличяющися столцов должно быть равно count($places) ';
 				return false;
 			}
@@ -70,29 +162,30 @@
 		}
 		protected function setDifferentSides($places)
 		{
-			if($this->is_debug){
-				$tmp_places = [];
-			}
 			foreach ($places as $place) {
 				$num_row = floor(($place - 1)/ $this->cols) + 1; 
-				$num_cell = $place - $this->cols * ($num_row - 1); 
+				$num_col = $place - $this->cols * ($num_row - 1); 
 				
 				if(!in_array($num_row, $this->different_rows)){
 					$this->different_rows[] = $num_row;
 				}
-				if(!in_array($num_cell, $this->different_cells)){
-					$this->different_cells[] = $num_cell;
+				if(!in_array($num_col, $this->different_cols)){
+					$this->different_cols[] = $num_col;
 				}
-				if($this->is_debug){
-					$tmp_places[] = 'r' . $num_row . '|c' . $num_cell;
-					var_dump('r' . $num_row . '|c' . $num_cell) ; echo '<br>';
-				}
+				$this->coords[$place] = [
+					'num_row' => $num_row,
+					'num_col' => $num_col,
+				];
 			}
 		}
 
+		protected function setActualMin()
+		{
+			$this->min = min($this->places);
+		}
 		public function getMin()
 		{
-			return min($this->places);
+			return $this->min;
 		}
 		public function getText()
 		{
@@ -121,19 +214,30 @@
 
 		public function getBgcolor()
 		{
-			return $this->bgcolor;
+			return $this->bgcolor ;
 		}
 
-		public function getColspan()
+		
+
+		public function getHeight()
 		{
-			return $this->colspan;
+			return $this->height;
 		}
 
-		public function getRowspan()
+		public function getWidth()
 		{
-			return $this->rowspan;
+			return $this->width;
 		}
 	
+		public function getTop()
+		{
+			return $this->top;
+		}
+
+		public function getLeft()
+		{
+			return $this->left;
+		}
 		public function isValid()
 		{
 			return !$this->errors;
--- src/http/grid.tpl
+++ src/http/grid.tpl
@@ -6,11 +6,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style>
 			div {
-				float: left;
+				
+			}
+			
+			div.container {
+				position: relative;
 			}
             .cell{
 				border: 1px solid #444;
-                padding: 1px;
+				position: absolute;
+				display: flex;
+				
                 /*
                     border-collapse: initial;
                 */
@@ -19,9 +25,22 @@
     </head>
     <body>
         <h1>ПЕРЕД ИСПОЛЬЗОВАНИЕМ ПОЧИНИТЬ</h1>
-			<div style="width: 300px; height: 300px;">
+			<div class="container" style="width: 300px; height: 300px;">
 				<?php foreach ($cells as $key_num => $cell) {?>
-					<div class="cell" style="width: 32%; height: 32%; "><?=$cell->getText() . " #" .  ($key_num)?></div>
+					<div class="cell"
+						style="
+							align-items: <?=$cell->getValign()?>;
+							width: <?=$cell->getWidth()?>;
+							height: <?=$cell->getHeight()?>;
+							top: <?=$cell->getTop()?>;
+							left: <?=$cell->getLeft()?>;
+							background-color:<?=$cell->getBgcolor()?>;
+							color:<?=$cell->getColor()?>;
+							justify-content:<?=$cell->getAlign()?>; 
+							" 
+						 >
+							<?=$cell->getText()?>
+					</div>
 				<?php } ?>
 			</div>
     </body>
